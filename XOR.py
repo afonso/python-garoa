86bae36de36f2007851b92ed4715f74735f6eaef
afonso <afonso@yack.com.br>
2013-12-02 20:43:09 -0200

    XOR arrumado cifra e decrifra

--- XOR.py
+++ XOR.py
@@ -3,27 +3,27 @@ Cifra XOR
 
 Aplica ciclicamente um XOR combinando os bytes da senha com os bytes do conteudo.
 
-	>>> s = 'pizza'
-	>>> t = 'Garoando'
-	>>> xor_decifra('pizza', xor_cifra('pizza', t))
-	'Garoando'
+    >>> s = 'pizza'
+    >>> t = 'Garoando'
+    >>> xor_decifra('pizza', xor_cifra('pizza', t))
+    'Garoando'
 
 """
 CODIF = 'utf-8'
 senha = 'garoas'
 import itertools
 
-def xor_bytes(senha, conteudo):
-	pares = zip(itertools.cycle(senha), conteudo)
-	return bytes(a ^ b for a, b in pares)
+def xor_bytes(senha:bytes, conteudo:bytes) -> bytes:
+    pares = zip(itertools.cycle(senha), conteudo)
+    return bytes(a ^ b for a, b in pares)
 
-def xor_cifra(senha, claro):
-	saida = xor_bytes(senha.encode(CODIF), claro.encode(CODIF))
-	return saida
+def xor_cifra(senha:str, claro:str) -> bytes:
+    saida = xor_bytes(senha.encode(CODIF), claro.encode(CODIF))
+    return saida
 
-def xor_decifra(senha, cifrado):
-	saida = xor_bytes(senha.encode(CODIF), cifrado)
-	return saida.decode(CODIF)
+def xor_decifra(senha:str, cifrado:bytes) -> str:
+    saida = xor_bytes(senha.encode(CODIF), cifrado)
+    return saida.decode(CODIF)
 
 def main():
     import sys
@@ -40,22 +40,26 @@ def main():
         print("modo usar: XOR.py [cifrar|decifrar] [senha] [nome_arquivo]")
         sys.exit(-1)
 
-    arq_entrada = open(nome_entrada, 'rt')
-
     if tipo == 'cifrar':
-    	print('Cifrando:', nome_entrada)
-    	for linha in arq_entrada:
-        	linha = linha.rstrip()
-        	saida = xor_cifra(senha, linha)
-        	print(saida)
-    elif tipo == 'decifrar':
-    	print('Decifrando:', nome_entrada)
-    	for linha in arq_entrada:
-	        linha = linha.rstrip()
-	        saida = xor_decifra(senha, linha)
-	        print(saida)
+        print('Cifrando:', nome_entrada)
 
-    arq_entrada.close()
+        arq_entrada = open(nome_entrada, 'rt', encoding = CODIF)
+        texto = arq_entrada.read()
+        arq_entrada.close()
+
+        saida = xor_cifra(senha, texto)
+
+        nome_arq_saida = nome_entrada + '.blob'
+        arq = open(nome_arq_saida, 'wb')
+        arq.write(saida)
+        arq.close()
+
+    elif tipo == 'decifrar':
+        print('Decifrando:', nome_entrada)
+        arq = open(nome_entrada, 'rb')
+        blob = arq.read()
+        texto = xor_decifra(senha, blob)
+        print(texto)
 
 if __name__ == '__main__':
     main()
